a7f882cc699628e634dc8397c71617f476ce1baf
Gustavo Fonseca <gus.fonnseca@gmail.com>
2021-08-20 22:28:29 -0300

    chore: chakra installed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import { useRef } from 'react';
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { Hydrate } from 'react-query/hydration';
-import '../styles/globals.css'
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   const queryClientRef = useRef<QueryClient>();
@@ -10,7 +10,12 @@ function MyApp({ Component, pageProps }: AppProps) {
     queryClientRef.current = new QueryClient();
   }
 
-  return ( <QueryClientProvider client={queryClientRef.current}>
-    <Hydrate state={pageProps.dehydratedState}><Component {...pageProps} /></Hydrate></QueryClientProvider>);
+  return (
+    <QueryClientProvider client={queryClientRef.current}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
-export default MyApp
+export default MyApp;
