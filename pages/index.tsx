92768900e70e21733f86cc4ae1b0be6928c3d0b4
Gustavo Fonseca <gus.fonnseca@gmail.com>
2021-08-30 22:46:47 -0300

    feat: icon and accordion

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import { Box } from '@chakra-ui/react';
 import { Form } from '../src/components/Form/Form';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -8,9 +9,19 @@ export default function Home() {
       <Head>
         <title>GameTask</title>
         <meta name="description" content="Sua vida no próximo nível" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/game-task-icon.svg" />
       </Head>
-      <Box as="header" bg="primary" width="100%" height={56}></Box>
+      <Box
+        as="header"
+        bg="primary"
+        width="100%"
+        height="80px"
+        display="flex"
+        alignItems="center"
+        padding="8px 16px"
+      >
+        <Image src="/game-task-icon.svg" height="60" width="60" />
+      </Box>
       <Box as="main" maxWidth="1024px" marginInline="auto">
         <Form />
       </Box>
--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,27 +1,46 @@
-import { Button, Input, Select, Stack, Textarea } from '@chakra-ui/react';
+import {
+  Accordion,
+  AccordionButton,
+  AccordionItem,
+  AccordionPanel,
+  Button,
+  Input,
+  Select,
+  Stack,
+  Textarea,
+} from '@chakra-ui/react';
 
 export function Form() {
   return (
-    <form>
-      <Stack spacing={4}>
-        <Input
-          placeholder="Qual tarefa será feita?"
-          aria-label="Digite o título da tarefa a ser feita"
-          size="lg"
-        />
-        <Textarea placeholder="Descrição da tarefa" size="lg" />
-        <Select placeholder="Select option" size="lg">
-          <option value="1" selected>
-            Fácil
-          </option>
-          <option value="2">Médio</option>
-          <option value="3">Difícil</option>
-        </Select>
-        <Input type="date" size="lg" />
-        <Button type="submit" bg="primary" size="lg">
-          Registrar
-        </Button>
-      </Stack>
-    </form>
+    <Accordion bg="whiteAlpha.200" borderRadius="8px" mt={8} allowToggle>
+      <AccordionItem border="none">
+        <AccordionButton alignItems="center">
+          Inserir nova tarefa
+        </AccordionButton>
+        <AccordionPanel>
+          <form>
+            <Stack spacing={4}>
+              <Input
+                placeholder="Qual tarefa será feita?"
+                aria-label="Digite o título da tarefa a ser feita"
+                size="lg"
+              />
+              <Textarea placeholder="Descrição da tarefa" size="lg" />
+              <Select placeholder="Selecione a dificuldade" size="lg">
+                <option value="1" selected>
+                  Fácil
+                </option>
+                <option value="2">Médio</option>
+                <option value="3">Difícil</option>
+              </Select>
+              <Input type="date" size="lg" />
+              <Button type="submit" bg="primary" size="lg">
+                Registrar
+              </Button>
+            </Stack>
+          </form>
+        </AccordionPanel>
+      </AccordionItem>
+    </Accordion>
   );
 }
